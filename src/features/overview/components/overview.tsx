d88536aff414ec7ee2ae0424af0965e90dbbec94
james-pike <jamesandrewpike@gmail.com>
2025-01-14 18:17:15 -0500

    commit

--- src/features/overview/components/overview.tsx
+++ src/features/overview/components/overview.tsx
@@ -31,7 +31,7 @@ export default function OverViewPage() {
             <TabsTrigger value='analytics'>KRC-20 Tokens</TabsTrigger>
           </TabsList>
           <TabsContent value='overview' className='space-y-4'>
-            <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4'>
+            <div className='grid grid-cols-2 gap-4 lg:grid-cols-4'>
               <Card>
                 <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
                   <CardTitle className='text-sm font-medium'>
